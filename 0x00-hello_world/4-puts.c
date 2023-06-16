017669bf1341180ccfb2ad9e9218559ad78894e6
Basmasebak <basmasebaq65@gmail.com>
2023-06-16 05:35:20 -0700

    l

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that a line with puts function
+ *
+ * return: 0 (Success)
+ */
+int main(void) /*a function to output the current text*/
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
