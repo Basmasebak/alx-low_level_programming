99430f7467b5613d3acbf171f450b3217feae963
Basmasebak <basmasebaq65@gmail.com>
2023-06-16 19:28:09 -0700

    k

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,21 +2,26 @@
 #include<time.h>
 #include<stdio.h>
 /**
-*main - is the function to define the vlue of n
-*
-*Return: Always 0 (Success)
-*
-*/
+ * main - assigns a random number to the variable n each time
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
+int last_digit;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 5)
-printf("%dLast digit of n and is greater than 5\n", n);
-else if (n == 0)
-printf("%d and is 0\n", n);
+
+last_digit = n % 10;
+printf("Last digit of %d is %d ", n, last_digit);
+
+if (last_digit > 5)
+printf("and is greater than 5\n");
+else if (last_digit == 0)
+printf("and is 0\n");
 else
-printf("%d and is less than 6 and not 0\n", n);
+printf("and is less than 6 and not 0\n");
 return (0);
 }
