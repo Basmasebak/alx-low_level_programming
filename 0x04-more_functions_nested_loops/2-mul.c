658e8b7b6850440c3b48be829186a149c3b08078
Basmasebak <basmasebaq65@gmail.com>
2023-08-25 01:59:16 -0700

    l

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to be checked
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int mul(int x ,int y)
+
+int _isdigit(int c)
 {
-    
-    return (x * y);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
